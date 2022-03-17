bcf61d45069a886b27b699b7f635168d71e223f8
John Pham <johnphammail@gmail.com>
2022-03-17 16:45:46 -0700

    Add Open Sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      display: ["Open Sans", "Segoe UI", "Tahoma", "sans-serif"],
+      body: ["Open Sans", "Segoe UI", "Tahoma", "sans-serif"],
+      sans: ["Open Sans", "Segoe UI", "Tahoma", "sans-serif"],
+    },
     extend: {
       colors: {
         gray100: "#f6f5f0",
         gray400: "#333333",
+      },
+      fontFamily: {
+        primary: "'Open Sans', 'Segoe UI', Tahoma, sans-serif",
       },
     },
   },
