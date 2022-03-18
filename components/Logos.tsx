b52f26a3c4a72091afcc0a093de72162a829006e
John Pham <johnphammail@gmail.com>
2022-03-18 13:57:26 -0700

    Add social proof grid

--- components/Logos.tsx
+++ components/Logos.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
+import githubLogo from '../public/social-proof/github.png'
 
 const Logos = () => {
-  return <div>Logos</div>
+  return (
+    <section className="grid grid-cols-4 gap-28 pb-32">
+      <Image src={githubLogo} alt="" />
+      <Image src={githubLogo} alt="" />
+      <Image src={githubLogo} alt="" />
+      <Image src={githubLogo} alt="" />
+      <Image src={githubLogo} alt="" />
+      <Image src={githubLogo} alt="" />
+      <Image src={githubLogo} alt="" />
+      <Image src={githubLogo} alt="" />
+      <Image src={githubLogo} alt="" />
+      <Image src={githubLogo} alt="" />
+    </section>
+  )
 }
 
 export default Logos
