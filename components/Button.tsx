d9eff5ff5487598a1a806c443e5f0a19b88a1b1e
John Pham <johnphammail@gmail.com>
2022-03-20 18:51:37 -0700

    Add link button

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,28 +3,45 @@ import React from 'react'
 type Props = {
   type?: 'primary' | 'default'
   endIcon?: React.ReactNode
+  asLink?: boolean
+  href?: string
 }
 
-const Button: React.FC<Props> = ({ children, type = 'default', endIcon }) => {
-  let baseStyles =
+const Button: React.FC<Props> = ({
+  children,
+  type = 'default',
+  endIcon,
+  asLink,
+  href,
+}) => {
+  let styles =
     'text-white font-bold bg-gray100 rounded-lg py-3 px-6 bg-opacity-40 uppercase text-[11px] leading-none flex items-center gap-2'
 
   switch (type) {
     case 'primary':
-      baseStyles = `${baseStyles} bg-gray400 bg-opacity-100 normal-case text-[12px]`
+      styles = `${styles} bg-gray400 bg-opacity-100 normal-case text-[12px]`
       break
     default:
     case 'default':
       break
   }
 
-  return (
-    <button className={baseStyles}>
+  const innerContents = (
+    <>
       <span>{children}</span>
 
       {endIcon && <span>{endIcon}</span>}
-    </button>
+    </>
   )
+
+  if (asLink && href) {
+    return (
+      <a href={href} className={styles}>
+        {innerContents}
+      </a>
+    )
+  }
+  return <button className={styles}>{innerContents}</button>
 }
 
 export default Button
