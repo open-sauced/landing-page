5a5844d81fd5dcee15d2d9330a0bba9c0dcecb79
shamimbinnur <sbnsbn00@gmail.com>
2022-05-05 17:27:02 +0600

    Connected sanity to GitHub mock & GitHub calender

---
+++ sanity/schemas/calender.js
@@ -0,0 +1,28 @@
+export default {
+    title: 'Calender',
+    name: 'calender',
+    type: 'document',
+    fields: [
+      {
+        type: 'string',
+        required: true,
+        title: 'Title',
+        name: 'title',
+        
+      },
+      {
+        type: 'string',
+        required: true,
+        title: 'Subtitle',
+        name: 'subtitle',
+        
+      },
+      {
+        type: 'image',
+        required: true,
+        title: 'Calender Image',
+        name: 'calenderImage',
+      },
+    ],
+  }
+  
--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -9,6 +9,7 @@ import user from './user'
 import navigation from './navigation'
 import seo from './seo'
 import githubMock from './githubMock'
+import calender from './calender'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -16,5 +17,5 @@ export default createSchema({
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([about, user, navigation, seo, githubMock]),
+  types: schemaTypes.concat([about, user, navigation, seo, githubMock, calender]),
 })
