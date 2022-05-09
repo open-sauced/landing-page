8975a67cb7df45d7ba2ab36880d741e70655149f
shamimbinnur <sbnsbn00@gmail.com>
2022-05-10 00:22:38 +0600

    Testimonial section connected to sanity

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -1,27 +1,24 @@
 import Image from 'next/image'
 import React, { FC } from 'react'
+import { SanityTestimonial } from '../types/schema'
 
 interface TestimonialProps{
-    testimonial:{
-        username: string,
-        date: string,
-        comment: string,
-        photo: string
-    }
+    testimonial : SanityTestimonial
 }
 
-const Testimonial:FC<TestimonialProps> = ({ testimonial:{username, date, comment, photo} }) => {
+const Testimonial:FC<TestimonialProps> = ({ testimonial: { twitterUsername, userImage, date, testimonial } }) => {
   return (
+
     <div className=' border-[2px] border-gray200 rounded-xl p-[15px] ' >
         <div className=' flex items-center gap-2 '>
             <div className=' shadow-md overflow-hidden rounded-full w-[36px] h-[36px]'>
-                <Image className=' rounded-full flex-[1] shadow-md  ' src={photo} width={36} height={36} />
+                <Image className=' rounded-full flex-[1] shadow-md  ' src={userImage as unknown as string} width={36} height={36} />
             </div>
-            <h6 className='text-gray400 text-[14px] flex-[3]' ><span className='font-bold text-twitterBlue '>{username}</span> <span/>@{username}. {date} </h6>
+            <h6 className='text-gray400 text-[14px] flex-[3]' ><span className='font-bold text-twitterBlue '>{twitterUsername}</span> <span/>@{twitterUsername}. {date} </h6>
         </div>
 
         <p className='text-gray400 text-[14px] pl-[44px]  '>
-            {comment}
+            {testimonial}
         </p>
 
 
