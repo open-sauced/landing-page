ce5cf6574a6b9a7be931d29bbe5e385dea0613bf
shamimbinnur <sbnsbn00@gmail.com>
2022-05-12 01:24:29 +0600

    New color scheme - issue no #9

--- components/BackgroundDrip.tsx
+++ components/BackgroundDrip.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
 import dripGraphic from '../public/background-drip-mirrored.svg'
+import dripGraphic2 from '../public/background-drip-mirrored2.svg'
 
 const BackgroundDrip: React.FC = ({ children }) => {
   return (
     <div className="h-fit tablet:h-[800px] pb-20 tablet:pb-0 relative">
       <div className="absolute w-[300vw] left-[138px] tablet:left-[148px] desktop:left-[20vw] overflow-hidden z-[-1]">
-        <Image src={dripGraphic} alt="" />
+        <Image src={dripGraphic2} alt="" />
       </div>
 
       {children}
