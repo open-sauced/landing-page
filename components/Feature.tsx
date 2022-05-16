8b9f21dbf00031b5839b00d160dc84ad9a60805b
shamimbinnur <sbnsbn00@gmail.com>
2022-05-16 18:56:40 +0600

    Added vertical icon next to feature title

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import React, { FC } from 'react'
 import { SanityFeature } from '../types/schema'
@@ -12,26 +11,24 @@ interface Props{
 const Feature:FC<Props> = ({feature: {title, subtitle, previewImage, slug }, index}) => {
   return (
     <div className=' px-2 tablet:px-20 ' >
-      <div className=' h-[1px]  bg-white ' ></div>
-      <div className={` flex px-8 gap-20 py-24 items-center  ${index % 2 == 1 ? 'tablet:flex-row-reverse' : 'tablet:flex-row'  } `} >
-        <div className=' hidden tablet:block flex-[1] '>
-          <img src={previewImage as unknown as string}/>
-        </div>
-        <div className=' text-white flex-[2] '>
-          <div className='flex items-center gap-4 '>
-            <img className='w-auto h-[50px] tablet:hidden ' src={previewImage as unknown as string}/>
-            <h1 className=' font-bold text-[30px] tablet:text-[36px]'>{title}</h1>
+        <div className=' h-[1px]  bg-white ' ></div>
+        <div className={` flex px-8 gap-20 py-24 items-center  ${index % 2 == 1 ? 'tablet:flex-row-reverse' : 'tablet:flex-row'  } `} >
+          <div className=' hidden tablet:block flex-[1] '>
+            <img src={previewImage as unknown as string}/>
           </div>
-
-          <p className=' text-[16px] tablet:text-[18px] py-[45px] '>
-            {subtitle}
-          </p>
-          
-          <div className='font-bold text-[16px] tablet:text-[18px] self-center '>
-            <Link href={"/feature/"+slug?.current as unknown as string}>Learn more &gt; </Link>
+          <div className=' text-white flex-[2] '>
+            <div className='flex items-center gap-4 '>
+              <img className='w-auto h-[50px] tablet:hidden ' src={previewImage as unknown as string}/>
+              <h1 className=' font-bold text-[30px] tablet:text-[36px]'>{title}</h1>
+            </div>
+            <p className=' text-[16px] tablet:text-[18px] py-[45px] '>
+              {subtitle}
+            </p>
+            <div className='font-bold text-[16px] tablet:text-[18px] self-center '>
+              <Link href={"/feature/"+slug?.current as unknown as string}>Learn more &gt; </Link>
+            </div>
           </div>
         </div>
-      </div>
     </div>
   )
 }
