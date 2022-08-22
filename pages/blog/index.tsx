3a064a6d8020a11ccbe48ea82ac19fd29fe3ff0f
shamimbinnnur <sbnsbn00@gmail.com>
2022-08-22 21:20:52 +0600

    fix: replaced all the single quotations

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -5,7 +5,7 @@ import Head from 'next/head'
 import { SanityAbout, SanityBlog, SanityNavigation, SanitySaucyBlog, SanitySeo } from '../../types/schema'
 import BlogBackgroundDrip from '../../components/BlogBackgroundDrip'
 import Navigation from '../../components/Header'
-import { FaOm, FaPizzaSlice } from 'react-icons/fa';
+import { FaPizzaSlice } from 'react-icons/fa';
 import { RiHashtag } from 'react-icons/ri';
 import { TiWorld } from 'react-icons/ti'
 import { BiTime } from 'react-icons/bi'
@@ -69,11 +69,11 @@ const index: NextPage<BlogPageProps> = ({ data: {seoData, homePageData, saucyBlo
           }
         />
 
-        <h1 className='text-center font-thin tracking-[1rem] text-[4rem] py-32 '>Blog</h1>
+        <h1 className="text-center font-thin tracking-[1rem] text-[4rem] py-32">Blog</h1>
         
-        <div className='py-[15px] flex items-center gap-x-[8px]'>
+        <div className="py-[15px] flex items-center gap-x-[8px]">
           <FaPizzaSlice size={25} className="text-gray-800"/>
-          <h6 className='font-semibold text-[25px] text-gray-800  '>Saucy blog</h6>
+          <h6 className="font-semibold text-[25px] text-gray-800">Saucy blog</h6>
         </div>
         <div className="flex flex-col tablet:flex-row gap-x-4 gap-y-10">
           {
@@ -81,43 +81,43 @@ const index: NextPage<BlogPageProps> = ({ data: {seoData, homePageData, saucyBlo
               <div key={_id} className=" flex-grow">
                 <Link passHref href={getBlogLink(isNative, slug?.current, blogUrl )}>
                   <div className="rounded-[15px] w-full h-auto overflow-hidden cursor-pointer">
-                    <Image objectFit='cover' alt={title} width={1100} height={900} layout='responsive' src={coverImage as unknown as string}/>
+                    <Image objectFit="cover" alt={title} width={1100} height={900} layout="responsive" src={coverImage as unknown as string}/>
                   </div>
                 </Link>
 
-                <div className='mt-[20px]'>
-                  <div className='flex gap-[8px] w-full flex-wrap items-center mt-[20px] tablet:mt-[50px] mb-[15px]'>
+                <div className="mt-[20px]">
+                  <div className="flex gap-[8px] w-full flex-wrap items-center mt-[20px] tablet:mt-[50px] mb-[15px]">
                     {/* <RiHashtag size={18}/> */}
                     {
                       topics?.slice(0,6).map( (topic, index) => (
-                        <div key={_id+index} className='flex items-center gap-[5px] min-h-[6px] overflow-hidden'>
-                          <div className='w-[6px] h-[6px] bg-orange-600 rounded-full'></div>
+                        <div key={_id+index} className="flex items-center gap-[5px] min-h-[6px] overflow-hidden">
+                          <div className="w-[6px] h-[6px] bg-orange-600 rounded-full"></div>
 
-                          <p className='text-sm'>{topic}</p>
+                          <p className="text-sm">{topic}</p>
                         </div>
                       ))
                     }
                   </div>
 
                   <Link href={getBlogLink(isNative, slug?.current, blogUrl )} passHref>
-                    <h6 className='font-bold text-[17px] text-gray-700 cursor-pointer'>{title}</h6>
+                    <h6 className="font-bold text-[17px] text-gray-700 cursor-pointer">{title}</h6>
                   </Link>
                   
-                  <div className='flex items-center gap-x-4 mt-[5px] mb-[15px]'>
-                    <div className='flex items-center gap-x-[5px]'>
+                  <div className="flex items-center gap-x-4 mt-[5px] mb-[15px]">
+                    <div className="flex items-center gap-x-[5px]">
                       <BiTime size={18} className="text-gray-600"/>
 
-                      <p className='text-gray-600 font-semibold text-[14px] '>{readTime} mins read</p>
+                      <p className="text-gray-600 font-semibold text-[14px]">{readTime} mins read</p>
                     </div>
-                    <div className='flex items-center gap-x-[5px]'>
+                    <div className="flex items-center gap-x-[5px]">
                       <BiUserCircle size={18} className="text-gray-600"/>
 
-                      <p className='text-gray-600 font-semibold text-[14px]'>{author}</p>
+                      <p className="text-gray-600 font-semibold text-[14px]">{author}</p>
                     </div>
                   </div>
                   
                   <Link href={getBlogLink(isNative, slug?.current, blogUrl )} passHref>
-                    <p className='text-[18px] text-gray-700 mt-[10px] tablet:mt-[30px] cursor-pointer'>{summary?.slice(0,180)}</p>
+                    <p className="text-[18px] text-gray-700 mt-[10px] tablet:mt-[30px] cursor-pointer">{summary?.slice(0,180)}</p>
                   </Link>
                 </div>
               </div>
@@ -126,76 +126,75 @@ const index: NextPage<BlogPageProps> = ({ data: {seoData, homePageData, saucyBlo
         </div>
         </BlogBackgroundDrip>
 
-        <div className='pt-[0] laptop:pt-[100px]'>
-          <div className='py-[15px] flex items-center gap-x-[8px]'>
+        <div className="pt-[0] laptop:pt-[100px]">
+          <div className="py-[15px] flex items-center gap-x-[8px]">
             <TiWorld size={25} className="text-gray-800"/>
-            <h6 className='font-semibold text-[25px] text-gray-800  '>Explore</h6>
+            <h6 className="font-semibold text-[25px] text-gray-800  ">Explore</h6>
           </div>
-          <div className='flex flex-col gap-y-6'>
+          <div className="flex flex-col gap-y-6">
             {
               blogs.map( ({ _id, title, summary, author, readTime, topics, blogUrl, isNative, slug, coverImage }) => (
-                <div key={_id} className='m' >
-                  <div className='flex flex-col tablet:flex-row gap-x-[20px]'>
-                    <div className='flex-1'>
+                <div key={_id}>
+                  <div className="flex flex-col tablet:flex-row gap-x-[20px]">
+                    <div className="flex-1">
                       <Link href={getBlogLink(isNative, slug?.current, blogUrl )} passHref>
                         <div className="rounded-[15px] cursor-pointer overflow-hidden bg-gray-100 max-h-[245px]">
-                          <Image objectFit='cover' alt={title} width={1100} height={900} layout='responsive' src={coverImage as unknown as string}/>
+                          <Image objectFit="cover" alt={title} width={1100} height={900} layout="responsive" src={coverImage as unknown as string}/>
                         </div>
                       </Link>
                       <div>
                       </div>
                     </div>
-                    <div className='flex-1 mt-[15px] tablet:mt-0'>
+                    <div className="flex-1 mt-[15px] tablet:mt-0">
                       <Link href={getBlogLink(isNative, slug?.current, blogUrl )} passHref>
-                        <h1 className='font-bold text-[20px] cursor-pointer text-gray-700'>{title}</h1>
+                        <h1 className="font-bold text-[20px] cursor-pointer text-gray-700">{title}</h1>
                       </Link>
 
-                      <div className='flex gap-[8px] items-center mt-[10px] mb-[20px]'>
+                      <div className="flex gap-[8px] items-center mt-[10px] mb-[20px]">
                         <RiHashtag size={18}/>
                           {
                             topics?.map( (topic, index) => (
-                              <div key={topic+index+1} className='flex items-center gap-[5px] min-h-[6px]'>
-                                <div className='w-[6px] h-[6px] bg-orange-600 rounded-full'></div>
+                              <div key={topic+index+1} className="flex items-center gap-[5px] min-h-[6px]">
+                                <div className="w-[6px] h-[6px] bg-orange-600 rounded-full"></div>
 
-                                <p className='text-sm'>{topic}</p>
+                                <p className="text-sm">{topic}</p>
                               </div>
                             ))
                           }
                       </div>
                       
                       <Link href={getBlogLink(isNative, slug?.current, blogUrl )} passHref>
-                        <p className='text-[18px] cursor-pointer hidden laptop:block '>{summary?.slice(0,180)}</p>
+                        <p className="text-[18px] cursor-pointer hidden laptop:block">{summary?.slice(0,180)}</p>
                       </Link>
 
-                      <div className='flex items-center gap-x-4 mt-[5px] mb-[15px]'>
-                        <div className='flex items-center gap-x-[5px] '>
+                      <div className="flex items-center gap-x-4 mt-[5px] mb-[15px]">
+                        <div className="flex items-center gap-x-[5px]">
                           <BiTime size={18} className="text-gray-600"/>
 
-                          <p className='text-gray-600 font-semibold text-[14px] '>{readTime} mins read</p>
+                          <p className="text-gray-600 font-semibold text-[14px]">{readTime} mins read</p>
                         </div>
-                        <div className='flex items-center gap-x-[5px]'>
+                        <div className="flex items-center gap-x-[5px]">
                           <BiUserCircle size={18} className="text-gray-600"/>
 
-                          <p className='text-gray-600 font-semibold text-[14px]'>{author}</p>
+                          <p className="text-gray-600 font-semibold text-[14px]">{author}</p>
                         </div>
                       </div>
                     </div>
                   </div>
                   <Link href={getBlogLink(isNative, slug?.current, blogUrl )} passHref>
-                    <p className='text-[18px] cursor-pointer hidden tablet:block laptop:hidden mt-[20px] mb-[30px]'>{summary?.slice(0,180)}</p>
+                    <p className="text-[18px] cursor-pointer hidden tablet:block laptop:hidden mt-[20px] mb-[30px]">{summary?.slice(0,180)}</p>
                   </Link>
                 </div>
               ))
             }
           </div>
         </div>
-
-        <div className='flex justify-center my-[30px]'>
-          <div className='flex gap-x-3 justify-center items-center h-[40px] w-[150px] cursor-pointer rounded-[20px] shadow-orange-600 shadow'>
-            <div className='w-[20px] h-[20px] rounded-full'>
+        <div className="flex justify-center my-[30px]">
+          <div className="flex gap-x-3 justify-center items-center h-[40px] w-[150px] cursor-pointer rounded-[20px] shadow-orange-600 shadow">
+            <div className="w-[20px] h-[20px] rounded-full">
               <Image alt='vector art' src={ellipseOrange}/>
             </div>
-            <p className='text-[15px] font-semibold text-gray-600'>Load more</p>
+            <p className="text-[15px] font-semibold text-gray-600">Load more</p>
           </div>
         </div>
       </div>
