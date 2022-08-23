78ed4425269c051b80ffe113df7001b025d290f5
shamimbinnnur <sbnsbn00@gmail.com>
2022-08-23 20:11:30 +0600

    style: backgroundDrip readjustment

--- components/BlogBackgroundDrip.tsx
+++ components/BlogBackgroundDrip.tsx
@@ -5,7 +5,7 @@ import dripGraphic2 from '../public/background-drip-mirrored2.svg'
 const BlogBackgroundDrip: React.FC = ({ children }) => {
   return (
     <div className="h-fit tablet:min-h-[800px] pb-20 tablet:pb-0 relative">
-      <div className="absolute w-[180vw] left-[138px] tablet:left-[148px] desktop:left-[40vw] _2k:left-[17vw]  overflow-hidden z-[-1]">
+      <div className="absolute w-[250vw] left-[180px] tablet:left-[300px] laptop:left-[500px] desktop:left-[45vw] _2k:left-[45vw]  overflow-hidden z-[-1]">
         <Image src={dripGraphic2} alt="graphics" />
       </div>
 
