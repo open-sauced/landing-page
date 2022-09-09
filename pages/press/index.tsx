0a11c7717f9146eb7a3a05903e89b95973023cf1
shamimbinnnur <sbnsbn00@gmail.com>
2022-09-10 00:05:25 +0600

    feat: title, subtitle, dos, dont

--- pages/press/index.tsx
+++ pages/press/index.tsx
@@ -4,6 +4,8 @@ import { getHomePageData, getSEOData, getPressData } from '../../lib/sanity'
 import { SanityAbout, SanityFooter, SanityNavigation, SanitySeo, SanityPress } from '../../types/schema'
 import Footer from '../../components/Footer'
 import Navigation from '../../components/Header'
+import Image from 'next/image'
+import { TbCheckbox, TbHandStop } from 'react-icons/tb';
 
  interface FeaturePageProps{
    data: {
@@ -17,7 +19,7 @@ import Navigation from '../../components/Header'
  }
 
 const index:FC<FeaturePageProps> = ({data: {seoData, homePageData, pressData}}) => {
-  
+  const { title, subtitle, dos, donts } = pressData
   return (
     <>
         <Head>
@@ -56,6 +58,58 @@ const index:FC<FeaturePageProps> = ({data: {seoData, homePageData, pressData}}) 
             />
 
             <footer className='mt-[100px]' >
+                <div className='flex flex-col text-center items-center justify-center gap-y-5'>
+                    <h1 className='text-gray-700 font-bold text-[2.8rem] '>{title}</h1>
+                    <p className='text-gray-700 font-normal text-[1.2rem]'>{subtitle}</p>
+                </div>
+
+                <div className='flex justify-between px-8'>
+                    {/* Do */}
+                    <div>
+                        <div className='flex items-center gap-4 mb-4 text-[1.5rem] font-semibold'>
+                            <div className='bg-green-400 rounded-[8px] p-1 bg-opacity-20 '>
+                                <TbCheckbox className='text-green-400'/>
+                            </div>
+                            <h6 className='text-gray-700'>You can</h6>
+                        </div>
+
+                        {
+                            dos &&
+                            dos.map((singleDo)=> (
+                                <div key={singleDo} className='flex flex-col gap-y-2'>
+                                    <div className='pl-2 flex items-center gap-x-4'>
+                                        <div className='bg-green-400 opacity-20 w-4 h-4 rounded-full'></div>
+                                        <p>{singleDo}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+
+                    {/* donts */}
+                    <div>
+                        <div className='flex items-center gap-4 mb-4 text-[1.5rem] font-semibold'>
+                            <div className='bg-red-400 rounded-[8px] p-1 bg-opacity-20 '>
+                                <TbHandStop className='text-red-400'/>
+                            </div>
+                            <h6 className='text-gray-700'>{"You can't"}</h6>
+                        </div>
+
+                        {
+                            donts &&
+                            donts.map((singleDont)=> (
+                                <div key={singleDont} className='flex flex-col gap-y-2'>
+                                    <div className='pl-2 flex items-center gap-x-4'>
+                                        <div className='bg-red-400 opacity-20 w-4 h-4 rounded-full'></div>
+                                        <p>{singleDont}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                    
+                </div>
+
             <Footer footer={homePageData.footer as unknown as SanityFooter[] || []} />
             </footer>
         </div>
