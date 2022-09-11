b89e8077115041fd15e71a56abd921e827f8ea0f
shamimbinnnur <sbnsbn00@gmail.com>
2022-09-11 06:26:57 +0600

    moved dos & donts section into components

--- pages/press/index.tsx
+++ pages/press/index.tsx
@@ -8,6 +8,7 @@ import { TbCheckbox, TbHandStop } from 'react-icons/tb';
 import { TiTick } from 'react-icons/ti';
 import { BsX } from 'react-icons/bs';
 import PermittedLogos from '../../components/PermittedLogos'
+import DosDonts from '../../components/DosDonts'
 
  interface FeaturePageProps{
    data: {
@@ -66,56 +67,7 @@ const index:FC<FeaturePageProps> = ({data: {seoData, homePageData, pressData}}) 
 
                 <PermittedLogos logos={openSaucedLogo as unknown as []}/>
 
-                <div className='flex flex-col tablet:flex-row tablet:justify-between gap-y-8 tablet:px-8'>
-                    {/* Do */}
-                    <div className='flex-1'>
-                        <div className='flex items-center gap-4 mb-4 text-[1.5rem] font-semibold'>
-                            <div className='bg-green-400 rounded-[8px] p-1 bg-opacity-20 '>
-                                <TbCheckbox className='text-green-400'/>
-                            </div>
-                            <h6 className='text-gray-700'>You can</h6>
-                        </div>
-
-                        {
-                            dos &&
-                            dos.map((singleDo)=> (
-                                <div key={singleDo} className='flex flex-col mb-2 gap-y-2'>
-                                    <div className='pl-2 flex gap-x-4'>
-                                        <div className='bg-green-400 mt-[6px] bg-opacity-20 w-4 h-4 rounded-full'>
-                                            <TiTick className='text-green-400'/>
-                                        </div>
-                                        <p className='m-0 p-0'>{singleDo}</p>
-                                    </div>
-                                </div>
-                            ))
-                        }
-                    </div>
-
-                    {/* donts */}
-                    <div className='flex-1'>
-                        <div className='flex items-center gap-4 mb-4 text-[1.5rem] font-semibold'>
-                            <div className='bg-red-400 rounded-[8px] p-1 bg-opacity-20 '>
-                                <TbHandStop className='text-red-400'/>
-                            </div>
-                            <h6 className='text-gray-700'>{"You can't"}</h6>
-                        </div>
-
-                        {
-                            donts &&
-                            donts.map((singleDont)=> (
-                                <div key={singleDont} className='flex flex-col mb-2 gap-y-2'>
-                                    <div className='pl-2 flex gap-x-4'>
-                                        <div className='bg-red-400 mt-[6px] bg-opacity-20 w-4 h-4 rounded-full'>
-                                            <BsX className='text-red-400'/>
-                                        </div>
-                                        <p>{singleDont}</p>
-                                    </div>
-                                </div>
-                            ))
-                        }
-                    </div>
-                    
-                </div>
+                <DosDonts dos={dos as unknown as []} donts={donts as unknown as []} />
 
             <footer className='mt-[100px]' >
                 <Footer footer={homePageData.footer as unknown as SanityFooter[] || []} />
