bc181aae9e8e42aff8fda279b924c25ff5adf175
shamimbinnnur <sbnsbn00@gmail.com>
2022-10-22 20:39:29 +0600

    footer

--- components/Footer2.tsx
+++ components/Footer2.tsx
@@ -1,36 +1,50 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import osWhiteLogo from '../public/osWhiteLogo.svg'
+import { BsDiscord, BsGithub, BsTwitter, BsYoutube } from 'react-icons/bs'
+import { FaDev } from 'react-icons/fa'
 
 const Footer2 = () => {
   return (
     <div className="bg-gray-800 py-12 px-28">
         <div className="mb-10">
-            <Image src={osWhiteLogo} />
+            <Image alt='OpenSauced' src={osWhiteLogo} />
         </div>
-        <div className="flex">
-            <div className="grow-0 px-4">
+        <div className="flex gap-x-10 gap-y-6 flex-wrap">
+            <div className="grow-0">
                 <div className="font-bold mb-2 text-gray-600 text-xs">SOCIAL</div>
-                <div className="flex gap-x-2">
-                    <div className="w-4 h-4 rounded-full bg-white"></div>
-                    <div className="w-4 h-4 rounded-full bg-white"></div>
-                    <div className="w-4 h-4 rounded-full bg-white"></div>
+                <div className="flex items-center gap-x-3">
+                    <a href="https://twitter.com/saucedopen">
+                        <BsTwitter className="text-white text-2xl" />
+                    </a>
+                    <a href="https://github.com/open-sauced">
+                        <BsGithub className="text-white text-2xl" />
+                    </a>
+                    <a href="https://www.youtube.com/c/OpenSauced">
+                        <BsYoutube className="text-white text-2xl" />
+                    </a>
+                    <a href="https://discord.gg/PhgxgXz8">
+                        <BsDiscord className="text-white text-2xl" />
+                    </a>
+                    <a href="https://dev.to/opensauced">
+                        <FaDev className="text-white text-2xl" />
+                    </a>
                 </div>
 
             </div>
             <div className="grow">
                 <div className="font-bold mb-2 text-gray-600 text-xs">MENU</div>
                 <ul className="text-sm text-gray-400">
-                    <li>About</li>
-                    <li>Blog</li>
-                    <li>Services</li>
+                    <li> <Link href="/blog">Blog</Link></li>
+                    <li> <Link href="/press">Press Kit</Link></li>
                 </ul>
             </div>
             <div className="grow">
                 <div className="font-bold mb-2 text-gray-600 text-xs">MORE SAUCE</div>
                 <ul className="text-sm text-gray-400">
-                    <li><a href="#">hot.opensauced.pizza</a></li>
-                    <li><a href="#">insight.opensauced.pizza</a></li>
+                    <li><Link href="hot.opensauced.pizza">hot.opensauced.pizza</Link></li>
+                    <li><Link href="insight.opensauced.pizza">insight.opensauced.pizza</Link></li>
                 </ul>
             </div>
             <div className="grow">
--- components/OpenSaucedLogos.tsx
+++ components/OpenSaucedLogos.tsx
@@ -14,7 +14,7 @@ interface Props {
 
 const OpenSaucedLogos:FC<Props> = ({logos}) => {
     return (
-    <div className="max-w-7xl px-6 tablet:px-0 mx-auto pt-16 tablet:pt-20 pb-20">
+    <div className="max-w-7xl px-6 tablet:px-2 mx-auto pt-16 tablet:pt-20 pb-20">
         <div className="flex justify-center gap-x-12 gap-y-24 flex-wrap">
             {
                 logos && 
