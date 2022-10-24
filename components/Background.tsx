2f8399e7e8652ebfe4902a5fec692cf1a5f99d63
shamimbinnnur <sbnsbn00@gmail.com>
2022-10-24 18:21:30 +0600

    made background stroke responsive

--- components/Background.tsx
+++ components/Background.tsx
@@ -10,13 +10,13 @@ const Background:FC = ({ children }) => {
         <div className="absolute -top-[44rem] tablet:-top-[42rem] -left-[28rem] largeTablet:-left-[16rem] laptop:-left-[14rem] desktop:-left-[10rem]">
             <Image src={upperStroke} />
         </div>
-        <div className="absolute top-[6rem] -right-[3rem]">
+        <div className="absolute top-[6rem] -right-[20rem] tablet:-right-[8rem] largeTablet:laptop:-right-[6rem] laptop:-right-[4rem] desktop:-right-[3rem]">
             <Image src={upperStroke} />
         </div>
-        <div className="absolute top-[50rem] -left-[11rem]">
+        <div className="absolute top-[50rem] -left-[28rem] largeTablet:-left-[18rem] laptop:-left-[15rem] desktop:-left-[11rem]">
             <Image src={leftSidePizzaStroke} />
         </div>
-        <div className="absolute top-[58rem] -right-[16rem]">
+        <div className="absolute top-[58rem] -right-[28rem] largeTablet:-right-[22rem] laptop:-right-[19rem] desktop:-right-[16rem]">
             <Image src={rightSidePizzaStroke} />
         </div>
 
