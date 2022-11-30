31f7f75f1b5ad9840b5e4540e0bb6237ad4c4e52
shamimbinnnur <sbnsbn00@gmail.com>
2022-12-01 00:08:24 +0600

    added footer

--- components/Footer2.tsx
+++ components/Footer2.tsx
@@ -7,13 +7,13 @@ import { FaDev } from 'react-icons/fa'
 
 const Footer2 = () => {
   return (
-    <div className="bg-gray-800 py-12 px-28">
+    <div className=" bg-brandRed bg-opacity-[7%] py-12 px-28">
         <div className="mb-10">
             <Image alt='OpenSauced' src={osWhiteLogo} />
         </div>
         <div className="flex gap-x-10 gap-y-6 flex-wrap">
             <div className="grow-0">
-                <div className="font-bold mb-2 text-gray-600 text-xs">SOCIAL</div>
+                <div className="font-bold mb-6 text-gray-600 text-xs">SOCIAL</div>
                 <div className="flex items-center gap-x-3">
                     <a href="https://twitter.com/saucedopen">
                         <BsTwitter className="text-white text-2xl" />
@@ -34,27 +34,27 @@ const Footer2 = () => {
 
             </div>
             <div className="grow">
-                <div className="font-bold mb-2 text-gray-600 text-xs">LINKS</div>
-                <ul className="text-sm text-gray-400">
+                <div className="font-bold mb-6 text-gray-600 text-xs">LINKS</div>
+                <ul className="text-sm flex flex-col gap-2 text-gray-400">
                     <li><Link href="/blog">Blog</Link></li>
                     <li><Link href="/press">Press Kit</Link></li>
                 </ul>
             </div>
             <div className="grow">
-                <div className="font-bold mb-2 text-gray-600 text-xs">ADDITIONAL RESOURCES</div>
-                <ul className="text-sm text-gray-400">
+                <div className="font-bold mb-6 text-gray-600 text-xs">ADDITIONAL RESOURCES</div>
+                <ul className="text-sm flex flex-col gap-2 text-gray-400">
                     <li><a href="https://hot.opensauced.pizza">hot.opensauced.pizza</a></li>
                     <li><a href="https://insights.opensauced.pizza">insights.opensauced.pizza</a></li>
                 </ul>
             </div>
             <div className="grow">
-                <div className="font-bold mb-2 text-gray-600 text-xs">CONTACT</div>
+                <div className="font-bold mb-6 text-gray-600 text-xs">CONTACT</div>
                 <div className="text-sm text-gray-400">
                     <p><a href="mailto:hello@opensauced.pizza">hello@opensauced.pizza</a></p>
                 </div>
             </div>
         </div>
-        <div className="grow flex justify-between mt-10 py-6 border-t-2 border-gray-700">
+        <div className="grow flex justify-between mt-16 py-6 border-t-2 border-gray-700">
             <div className="text-sm text-gray-500">C 2022 Open Sauced, LLC. All rights reserved.</div>
             <div className="text-sm text-gray-500">
                 <ul className="flex gap-2">
