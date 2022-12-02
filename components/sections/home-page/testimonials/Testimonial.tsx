4319c32848dd87dbc5abc1e634ca90b1dcc27e81
Stefan Vucic <stefan.vucic@neotechsolutions.org>
2022-12-02 01:22:03 +0100

    Testemonials section mobile slider implemented

--- components/sections/home-page/testimonials/Testimonial.tsx
+++ components/sections/home-page/testimonials/Testimonial.tsx
@@ -12,46 +12,51 @@ const Testimonial: FC<TestimonialProps> = ({
     twitterUsername,
     twitterName,
     userImage,
-    date,
     testimonial,
     tweetLink,
   },
 }) => {
   return (
-    <GradientBorderWrapper style={{ borderRadius: '8px', width: '100%' }}>
-      <div className="h-fit min-h-[272px] w-full p-10 bg-darkBG rounded-lg flex flex-col">
-        <div className="flex-grow">
-          <Typography>{testimonial}</Typography>
-        </div>
+    <div>
+      <GradientBorderWrapper style={{ borderRadius: '8px', width: '100%' }}>
+        <div className="h-fit min-h-[320px] w-full p-10 bg-darkBG rounded-lg flex flex-col largeTablet:min-h-[300px]">
+          <div className="flex-grow">
+            <LocalTypography>{testimonial}</LocalTypography>
+          </div>
 
-        <div className=" flex items-center gap-2 ">
-          <GradientBorderWrapper style={{ borderRadius: '50%' }}>
-            <div className=" shadow-md overflow-hidden rounded-full w-[72px] h-[72px]">
-              <Image
-                className=" rounded-full flex-[1] shadow-md  "
-                alt={twitterUsername + "'s photo"}
-                src={userImage as unknown as string}
-                width={72}
-                height={72}
-              />
+          <div className=" flex items-center gap-2 ">
+            <GradientBorderWrapper style={{ borderRadius: '50%' }}>
+              <div className=" shadow-md overflow-hidden rounded-full w-[72px] h-[72px]">
+                <Image
+                  className=" rounded-full flex-[1] shadow-md  "
+                  alt={twitterUsername + "'s photo"}
+                  src={userImage as unknown as string}
+                  width={72}
+                  height={72}
+                />
+              </div>
+            </GradientBorderWrapper>
+
+            <div className="pl-6">
+              <h6 className="text-gray400 font-bold text-[14px] flex-[3]">
+                <LocalTypography variant="username">
+                  {twitterName}{' '}
+                </LocalTypography>
+                <a
+                  className=" hover:text-gray150 font-normal text-[14px] tracking-[-4%] "
+                  href={tweetLink as unknown as string}
+                >
+                  <LocalTypography variant="twitter">
+                    @{twitterUsername}{' '}
+                  </LocalTypography>
+                </a>
+                <span />
+              </h6>
             </div>
-          </GradientBorderWrapper>
-
-          <div className="pl-6">
-            <h6 className="text-gray400 font-bold text-[14px] flex-[3]">
-              <Typography variant="username">{twitterName} </Typography>
-              <a
-                className=" hover:text-gray150 font-normal text-[14px] tracking-[-4%] "
-                href={tweetLink as unknown as string}
-              >
-                <Typography variant="twitter">@{twitterUsername} </Typography>
-              </a>
-              <span />
-            </h6>
           </div>
         </div>
-      </div>
-    </GradientBorderWrapper>
+      </GradientBorderWrapper>
+    </div>
   )
 }
 
@@ -59,7 +64,7 @@ interface TypographyProps {
   variant?: 'content' | 'username' | 'twitter'
 }
 
-const Typography: FC<TypographyProps> = ({
+const LocalTypography: FC<TypographyProps> = ({
   children,
   variant = 'content',
 }): React.ReactElement => {
--- components/sections/home-page/testimonials/Testimonials.tsx
+++ components/sections/home-page/testimonials/Testimonials.tsx
@@ -1,14 +1,39 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { SanityTestimonial } from '../../../../types/schema'
 import SectionWrapper from '../../../common/layout/SectionWrapper'
 import { Heading, Typography } from '../../../common/text'
 import Testimonial from './Testimonial'
+import 'react-slideshow-image/dist/styles.css'
+import { Slide, SlideshowRef } from 'react-slideshow-image'
 
 interface TestimonialsProps {
   data: SanityTestimonial[]
 }
 
 const Testimonials: FC<TestimonialsProps> = ({ data }) => {
+  const [slideIndex, setSlideIndex] = useState(0)
+  const slideRef = React.createRef<SlideshowRef>()
+
+  const handleSlideChange = (index: number) => {
+    setSlideIndex(index)
+  }
+
+  const properties = {
+    duration: 5000,
+    autoplay: false,
+    transitionDuration: 500,
+    arrows: false,
+    infinite: true,
+    easing: 'ease',
+    indicators: (i?: number) => {
+      const backgroundStyle = i === slideIndex ? 'bg-[#F87216]' : 'bg-slate-700'
+      return (
+        <div
+          className={`mr-3 h-3 w-3 rounded-[50%] mt-4 cursor-pointer ${backgroundStyle}`}
+        ></div>
+      )
+    },
+  }
   return (
     <SectionWrapper pb={215}>
       <Typography variant="preHeading" alignSmall="center">
@@ -18,13 +43,34 @@ const Testimonials: FC<TestimonialsProps> = ({ data }) => {
         The value of $yOpen Source$y
       </Heading>
 
-      <div className="w-full mt-[46px] grid grid-cols-1 largeTablet:grid-cols-3 gap-9 largeTablet:mt-24  ">
+      <div className="w-full mt-[46px] grid-cols-1 largeTablet:grid-cols-3 gap-9 largeTablet:mt-24 hidden largeTablet:grid ">
         {data.map((testimonial) => (
           <Testimonial
             key={testimonial.twitterUsername}
             testimonial={testimonial as unknown as SanityTestimonial}
           />
         ))}
+      </div>
+      <div className="w-full mt-[46px] largeTablet:mt-24 largeTablet:hidden ">
+        <Slide
+          ref={slideRef}
+          {...properties}
+          onChange={(x, y) => handleSlideChange(y)}
+        >
+          {data.map((testimonial) => (
+            <div
+              key={testimonial.twitterUsername}
+              style={{
+                padding: '0px 5px',
+                boxSizing: 'border-box',
+              }}
+            >
+              <Testimonial
+                testimonial={testimonial as unknown as SanityTestimonial}
+              />
+            </div>
+          ))}
+        </Slide>
       </div>
     </SectionWrapper>
   )
