1d90a0e3e2dd04f6f32a713fc5f91d00d9d1a040
Stefan Vucic <stefan.vucic@neotechsolutions.org>
2022-12-12 22:57:27 +0100

    Issue #80 resolved

--- components/sections/Subscribe.tsx
+++ components/sections/Subscribe.tsx
@@ -10,7 +10,7 @@ import StrokeMobile3 from '../../public/background-strokes/stroke_mobile_3.svg'
 
 const Subscribe: FC = (): ReactElement => {
   const inputValue = useRef<HTMLInputElement>(null)
-  const [success, setSuccess] = useState(false);
+  const [success, setSuccess] = useState(false)
 
   const handleSubscribe = () => {
     const email = inputValue.current?.value
@@ -24,23 +24,24 @@ const Subscribe: FC = (): ReactElement => {
         return alert('Not a valid Email')
       }
       fetch('/favicon.svg', {
-        method: 'POST', 
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, 
-        body: `form-name=newsletter&email=${email}`, 
-
-       })
-       .then(responseFromServer => console.log('responseFromServer',responseFromServer))
-       .catch(networkError => console.error('networkError',networkError))
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: `form-name=newsletter&email=${email}`,
+      })
+        .then((responseFromServer) =>
+          console.log('responseFromServer', responseFromServer)
+        )
+        .catch((networkError) => console.error('networkError', networkError))
     } else {
       alert('Email is required!')
     }
   }
 
   useEffect(() => {
-    if ( window.location.search.includes('success=true') ) {
-      setSuccess(true);
+    if (window.location.search.includes('success=true')) {
+      setSuccess(true)
     }
-  }, []);
+  }, [])
 
   return (
     <SectionWrapper pbs={120} pb={310}>
@@ -53,18 +54,39 @@ const Subscribe: FC = (): ReactElement => {
             Stay up to date with the latest OpenSauced news and trends.
           </Typography>
         </div>
+        <div className="absolute hidden largeTablet:block largeTablet:bottom-[36rem] largeTablet:left-0">
+          <Image alt="Doodles" src={StrokeL3} />
+        </div>
+
+        <div className="absolute hidden largeTablet:block largeTablet:bottom-[35rem] largeTablet:right-0">
+          <Image alt="Doodles" src={StrokeR3} />
+        </div>
+
+        <div className="absolute bottom-[51rem] right-0  largeTablet:hidden">
+          <Image alt="Doodles" src={StrokeMobile2} />
+        </div>
+
+        <div className="absolute bottom-[32rem] left-0  largeTablet:hidden">
+          <Image alt="Doodles" src={StrokeMobile3} />
+        </div>
         <GradientBorderWrapper>
-          <form 
+          <form
             className="relative box-border px-4 flex items-center w-[280px] h-[38px] text-[#FEEADD] pr-4 py-3 text-sm font-medium bg-[#211E1C] rounded-md largeTablet:w-[394px]"
-            name="newsletter" 
+            name="newsletter"
             action="?success=true"
-            data-netlify="true" 
+            data-netlify="true"
             data-netlify-honeypot="bot-field"
+            autoComplete="off"
           >
             <div hidden aria-hidden="true">
               <label>
                 Try your luck
-                <input type="hidden" value="bot-field" name="newsletter" />
+                <input
+                  type="hidden"
+                  value="bot-field"
+                  name="newsletter"
+                  autoComplete="off"
+                />
               </label>
             </div>
             <input
@@ -82,27 +104,8 @@ const Subscribe: FC = (): ReactElement => {
               Subscribe
             </button>
           </form>
-          {success && (
-            <p>
-              Successfully submitted form!
-            </p>
-          )}
+          {success && <p>Successfully submitted form!</p>}
         </GradientBorderWrapper>
-        <div className="absolute hidden largeTablet:block largeTablet:bottom-[36rem] largeTablet:left-0">
-          <Image alt="Doodles" src={StrokeL3} />
-        </div>
-
-        <div className="absolute hidden largeTablet:block largeTablet:bottom-[35rem] largeTablet:right-0">
-          <Image alt="Doodles" src={StrokeR3} />
-        </div>
-
-        <div className="absolute bottom-[55rem] right-0  largeTablet:hidden">
-          <Image alt="Doodles" src={StrokeMobile2} />
-        </div>
-
-        <div className="absolute bottom-[34rem] left-0  largeTablet:hidden">
-          <Image alt="Doodles" src={StrokeMobile3} />
-        </div>
       </div>
     </SectionWrapper>
   )
