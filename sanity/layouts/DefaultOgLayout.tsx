a2121431d492191956a2605104c6e5d8687b0724
shamimbinnnur <sbnsbn00@gmail.com>
2023-02-05 23:08:10 +0600

    Resized social card

--- sanity/layouts/DefaultOgLayout.tsx
+++ sanity/layouts/DefaultOgLayout.tsx
@@ -26,16 +26,16 @@ const DefaultOgLayout = {
     }
   },
   dimensions: {
-    width: 1920,
-    height: 1080,
+    width: 1344,
+    height: 756,
   },
   component: ({ title, subtitle }) => (
     <div style={{ width: "100%", height:"100%", position: "relative"}}>
       <div style={{ position: "absolute", top:"0", right:"0" }}>
         <Background/>
       </div>
-      <div style={{ position:"relative", height:"100%", paddingRight:"200px", paddingLeft:"200px", wordWrap: "break-word", display: "flex", flexDirection: "column", justifyItems:"center", justifyContent: "center" }}>
-        <h1 style={{ color: "#FFF9ED", fontWeight: "700", fontSize: "120px", lineHeight: "96px" ,margin:"20px 0 20px 0" }}>{title || ''}</h1>
+      <div style={{ position:"relative", height:"100%", paddingRight:"152px", paddingLeft:"152px", paddingTop:"80px", wordWrap: "break-word", display: "flex", flexDirection: "column", justifyItems:"center", justifyContent: "center" }}>
+        <h1 style={{ color: "#FFF9ED", fontWeight: "700", fontSize: "120px", lineHeight: "100px" ,margin:"20px 0 20px 0" }}>{title || ''}</h1>
         <p style={{ color: "#FFF9ED", fontWeight: "400", fontSize: "36px" }}>{subtitle || ''}</p>
       </div>
     </div>
