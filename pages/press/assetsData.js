927a47393184174a53ab29c3d0703c518ac5b208
shamimbinnnur <sbnsbn00@gmail.com>
2023-03-10 19:41:27 +0600

    .

--- pages/press/assetsData.js
+++ pages/press/assetsData.js
@@ -2,7 +2,7 @@ export const openSaucedlogos = [
   {
     id: 1,
     title: "Orange and White",
-    description: "Please use this logo in cases where the full logo(Orange and Black, Full White, Full Black) won’t work.",
+    description: "Please use this logo in cases where the full logo (Orange and Black, Full White, Full Black) won’t work.",
     svgLogo: "/brandLogos/full_orange_white.svg",
     pngLogo: "/brandLogos/full_orange_white.png",
     isBlackBG: true,
@@ -10,7 +10,7 @@ export const openSaucedlogos = [
   {
     id: 2,
     title: "Orange and Black",
-    description: "Please use this logo in cases where the full logo(Orange and White, Full White, Full Black) won’t work.",
+    description: "Please use this logo in cases where the full logo (Orange and White, Full White, Full Black) won’t work.",
     svgLogo: "/brandLogos/full_orange_black.svg",
     pngLogo: "/brandLogos/full_orange_black.png",
     isBlackBG: false,
@@ -26,7 +26,7 @@ export const openSaucedlogos = [
   {
     id: 4,
     title: "Full Black",
-    description: "Please use this logo in cases where the full logo(Orange and White, Orange and Black, Full White) won’t work.",
+    description: "Please use this logo in cases where the full logo (Orange and White, Orange and Black, Full White) won’t work.",
     svgLogo: "/brandLogos/full_black.svg",
     pngLogo: "/brandLogos/full_black.png",
     isBlackBG: false,  
@@ -34,7 +34,7 @@ export const openSaucedlogos = [
   {
     id: 5,
     title: "Orange Slice",
-    description: "Please use this logo in cases where slice(White, Black) won't work.",
+    description: "Please use this logo in cases where slice (White, Black) won't work.",
     svgLogo: "/brandLogos/icon_orange.svg",
     pngLogo: "/brandLogos/icon_orange.png",
     isBlackBG: false,
@@ -42,7 +42,7 @@ export const openSaucedlogos = [
   {
     id: 6,
     title: "Black Slice",
-    description: "Please use this logo in cases where slice(White, Orange) won’t work.",
+    description: "Please use this logo in cases where slice (White, Orange) won’t work.",
     svgLogo: "/brandLogos/icon_black.svg",
     pngLogo: "/brandLogos/icon_black.png",
     isBlackBG: false,
@@ -50,7 +50,7 @@ export const openSaucedlogos = [
   {
     id: 7,
     title: "White Slice",
-    description: "Please use this logo in cases where slice(Orange, Black) won’t work.",
+    description: "Please use this logo in cases where slice (Orange, Black) won’t work.",
     svgLogo: "/brandLogos/icon_white.svg",
     pngLogo: "/brandLogos/icon_white.png",
     isBlackBG: true,
