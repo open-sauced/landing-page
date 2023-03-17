7bd2051cc2af6fb3d3371563c6334fe0e0fa1bd5
Shamim Bin Nur <48399373+shamimbinnur@users.noreply.github.com>
2023-03-17 22:45:07 +0600

    feat: add rss feed to blog post. (#115)
    * feat: added rss
    
    * add actual site url
    
    * removed generated rss xml
    
    * ignored feed.xml for git
    
    * .
    
    * test
    
    * Refactored code to utilize API for handling RSS functionality.
    
    * Changed git ignore file

---
+++ pages/api/rss/blog.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import RSS from 'rss';
+import { getAllBlogs } from '../../../lib/sanity';
+
+const site_url = 'https://opensauced.pizza';
+
+export default async function handler (
+  req: NextApiRequest,
+  res: NextApiResponse
+  ) {
+    try {
+      const blogs = await getAllBlogs();
+     
+      const feedOptions = {
+        title: 'OpenSauced Blog',
+        description: 'Welcome to OpenSauced blog!',
+        site_url: site_url,
+        feed_url: `${site_url}/api/rss/blog`,
+        image_url: `${site_url}/rss/logo.png`,
+        pubDate: new Date(),
+        copyright: `All rights reserved ${new Date().getFullYear()}, OpenSauced`,
+      };
+      
+      const feed = new RSS(feedOptions);
+    
+      blogs.map((post) => {
+        feed.item({
+          title: post.title as string,
+          description: post.summary as string,
+          url: `${site_url}/blog/${post.slug?.current}`,
+          date: post._createdAt,
+          author: post.author
+        });
+      });
+
+      res.setHeader('Content-Type', 'text/xml');
+      res.status(200).send(feed.xml({ indent: true }));
+      
+    } catch (error) {
+      res.status(500).send('Internal Server Error');
+    }
+}
