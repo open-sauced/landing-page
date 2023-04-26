8f5371d477b845564958e1b77a83c23b85bb0600
Shamim Bin Nur <sbnsbn00@gmail.com>
2023-04-26 16:52:26 +0600

    final touch

--- components/sections/home-page/Newsletter.tsx
+++ components/sections/home-page/Newsletter.tsx
@@ -4,15 +4,32 @@ import { Heading, Typography } from '../../common/text'
 import GradientBorderWrapper from '../../common/GradientBorderWrapper'
 import Image from 'next/image'
 
-import StrokeR3 from '../../../public/background-strokes/stroke_r_3.svg'
-import StrokeL3 from '../../../public/background-strokes/stroke_l_3.svg'
+import StrokeR3 from '../../../public/_3_rightStroke.svg'
+import StrokeL3 from '../../../public/_3_leftStroke.svg'
 import StrokeMobile2 from '../../../public/background-strokes/stroke_mobile_2.svg'
 import StrokeMobile3 from '../../../public/background-strokes/stroke_mobile_3.svg'
 
 const Newsletter = () => {
   return (
     <SectionWrapper pt={100} pb={250}>
-      <div className="flex flex-col items-center gap-y-8">
+        <div className="relative w-screen">
+          <div className="absolute largeTablet:block top-[-14rem] largeTablet:-top-[10rem] left-[-30rem] largeTablet:left-[-15rem]">
+            <Image alt="Doodles" src={StrokeL3} />
+          </div>
+
+          <div className="absolute largeTablet:block top-[-14rem] largeTablet:-top-[22rem] right-[-34rem] largeTablet:-right-[32rem]">
+            <Image alt="Doodles" src={StrokeR3} />
+          </div>
+
+          <div className="absolute bottom-[51rem] right-0  largeTablet:hidden">
+            <Image alt="Doodles" src={StrokeMobile2} />
+          </div>
+
+          <div className="absolute bottom-[32rem] left-0  largeTablet:hidden">
+            <Image alt="Doodles" src={StrokeMobile3} />
+          </div>
+        <div className="flex flex-col items-center gap-y-8">
+          
         <Heading alignSmall="center">
           $orange-to-yellowThe fast track$orange-to-yellow to open source
         </Heading>
@@ -38,6 +55,7 @@ const Newsletter = () => {
             </div>
           </GradientBorderWrapper>
         </form>
+      </div>
       </div>
     </SectionWrapper>
   )
