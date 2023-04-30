4d881e40652488cb73f26d2887d701fc85a24fe0
Shamim Bin Nur <sbnsbn00@gmail.com>
2023-05-01 01:32:17 +0600

    Implemented hero section

--- components/sections/home-page/Hero.tsx
+++ components/sections/home-page/Hero.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { SanityHomePage } from '../../../types/schema'
 import SectionWrapper from '../../common/layout/SectionWrapper'
 import ContainerWithLine from '../../common/ContainerWithLine'
+import { Heading, Typography } from '../../common/text'
+import { Button } from '../../common'
+import DecoratedText from '../../common/text/utils/DecoratedText'
+import Link from 'next/link'
 
 interface HeroProps {
   data: SanityHomePage['hero']
@@ -9,8 +13,58 @@ interface HeroProps {
 
 const Hero = ({ data }: HeroProps) => {
   return (
-    <SectionWrapper>
+    <SectionWrapper pt={115} pts={60} >
       <ContainerWithLine>
+        <main>
+          <div className="flex largeTablet:items-center h-full flex-col gap-x-6 gap-y-10 largeTablet:flex-row mx-6">
+            <div className="flex-1 ">
+              <Typography alignSmall="left" alignLarge="left" variant="preHeading">
+                YOU GOT THIS
+              </Typography>
+
+              <div className="relative">
+                <img className="absolute -left-[68px] -top-[50px]" src="/icons/journey_ball.svg" alt="Starting point"/>
+
+                <Heading component="h1" alignSmall="left" alignLarge="left">
+                  Your next open source journey $orange-to-yellowstarts here$orange-to-yellow
+                </Heading>
+              </div>
+              
+              <div className="py-10">
+                <Typography alignLarge="left" alignSmall="left" variant="body3">
+                  Start the path to your next contribution and join the global community of open source developers making an impact around the world.
+                </Typography>
+              </div>
+
+              <div className="flex gap-4 flex-col largeTablet:flex-row">
+              {data?.cta?.map((item, i) => (
+                <Button key={i} href={item.ctaLink} borderVariant={ i !== 0 ? "gray" : "neon" }>
+                  {item.ctaLabel}
+                </Button>
+              ))}
+              </div>
+              
+              <div className="pt-6 pb-10">
+                <Typography alignLarge="left" alignSmall="left" variant="body3">
+                  Working with team?
+                  <span>
+                    <Link href="/enterprise">
+                      <a>
+                        <DecoratedText content="$orange-to-yellow Click here$orange-to-yellow"/>
+                      </a>
+                    </Link>
+                  </span> 
+                </Typography>
+              </div>
+            </div>
+            
+            <div className="flex-1 ">
+              <div className="">
+                <img className="relative scale-105 largeTablet:scale-110 -right-4" src="/hero.svg" alt="" />
+              </div>
+            </div>
+          </div>
+        </main>
       </ContainerWithLine>
     </SectionWrapper>
   )
