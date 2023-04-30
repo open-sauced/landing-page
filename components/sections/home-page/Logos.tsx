d78c78975e8bfb0fe1df8be24a642289fe7d495a
Shamim Bin Nur <sbnsbn00@gmail.com>
2023-04-30 22:06:25 +0600

    Removed github mock image

--- components/sections/home-page/Logos.tsx
+++ components/sections/home-page/Logos.tsx
@@ -8,29 +8,7 @@ interface LogosProps {
 
 const Logos = ({ data }: LogosProps) => {
   return (
-    <SectionWrapper direction="row" pb={150} pbs={150}>
-      <div className="w-full max-w-[900px] grid grid-cols-3 gap-x-10 gap-y-8 justify-center items-center content-center largeTablet:mt-0 largeTablet:grid-cols-6">
-        {data?.map((item) => {
-          const href = item.website || ''
-          const src = item.logo || ''
-          return (
-            <a
-              key={item.name}
-              href={href}
-              target="_blank"
-              rel="noreferrer"
-              className="relative h-[18px] largeTablet:h-6 mx-auto"
-            >
-              <img
-                className="h-full"
-                src={src as string}
-                alt={item.name}
-                loading="lazy"
-              />
-            </a>
-          )
-        })}
-      </div>
+    <SectionWrapper>
     </SectionWrapper>
   )
 }
