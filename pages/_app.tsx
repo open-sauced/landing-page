78cef3abfea890330c2d69ec9171964db4b9a077
Brian 'bdougie' Douglas <bdougie@no-reply.github.com>
2023-05-22 13:54:52 -0700

    adds posthog middleware

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,42 @@
 import '../styles/globals.css'
 import "../styles/blog_text_content_wrapper.css"
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: 'https://app.posthog.com',
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug()
+    }
+  })
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog?.capture('$pageview')
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
+  return (
+    <PostHogProvider client={posthog}>
+
+  <Component {...pageProps} />
+  </PostHogProvider>
+  )
 }
 
 export default MyApp
