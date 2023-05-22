d5e1ff7a853601cb781c60d38774ec975296ca37
Brian 'bdougie' Douglas <bdougie@no-reply.github.com>
2023-05-22 14:03:02 -0700

    undefined fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,8 +9,8 @@ import { PostHogProvider } from 'posthog-js/react'
 
 // Check that PostHog is client-side (used to handle Next.js SSR)
 if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: 'https://app.posthog.com',
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
     // Enable debug mode in development
     loaded: (posthog) => {
       if (process.env.NODE_ENV === 'development') posthog.debug()
