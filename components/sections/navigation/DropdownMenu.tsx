b68dd26d2e27f651de2db6be6a2e439d1f527609
dev-phantom <godsfavour1975@gmail.com>
2023-09-14 18:26:12 +0100

    test 3

--- components/sections/navigation/DropdownMenu.tsx
+++ components/sections/navigation/DropdownMenu.tsx
@@ -1,21 +1,21 @@
-import { Fragment, useState } from 'react'
-import Image from 'next/image'
-import { Menu, Transition } from '@headlessui/react'
-import Link from 'next/link'
+import { Fragment, useState } from 'react';
+import Image from 'next/image';
+import { Menu, Transition } from '@headlessui/react';
+import Link from 'next/link';
 
-import SocialLinks from '../../common/SocialLinks'
-import MobileMenu from '../../../public/mobile_menu.svg'
-import MobileCloseMenu from '../../../public/close_btn.svg'
+import SocialLinks from '../../common/SocialLinks';
+import MobileMenu from '../../../public/mobile_menu.svg';
+import MobileCloseMenu from '../../../public/close_btn.svg';
 
 interface MenuItem {
-  label: string
-  href: string
+  label: string;
+  href: string;
 }
 
 interface DropdownMenuProps {
-  menuItems: MenuItem[]
-  className?: string
-  label: string
+  menuItems: MenuItem[];
+  className?: string;
+  label: string;
 }
 
 const DropdownMenu = ({ menuItems, className, label }: DropdownMenuProps) => {
@@ -42,32 +42,33 @@ const DropdownMenu = ({ menuItems, className, label }: DropdownMenuProps) => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        {/* Place Menu.Items here */}
-        <Menu.Items className="absolute z-50 h-fit min-h-[470px] right-6 left-6 mt-6 bg-gradient-to-r from-[#ED5432] to-[#EDA232] py-9 px-7 rounded-lg">
-          <p className="font-bold text-textPrimary text-xs opacity-70 tracking-[0.2em] pb-8">
-            MENU
-          </p>
-          <div className="flex flex-col gap-y-8 pb-14">
-            {menuItems.map(({ label, href }) => (
-              <Menu.Item key={label}>
-                <Link href={href} passHref>
-                  <span className="font-bold text-textPrimary text-lg">
-                    {label}
-                  </span>
-                </Link>
-              </Menu.Item>
-            ))}
-          </div>
-          <div>
-            <p className="font-bold text-textPrimary text-xs opacity-70 tracking-[0.2em] pb-10">
-              SOCIALS
+        {(isOpen) && (
+          <Menu.Items className="absolute z-50 h-fit min-h-[470px] right-6 left-6 mt-6 bg-gradient-to-r from-[#ED5432] to-[#EDA232] py-9 px-7 rounded-lg">
+            <p className="font-bold text-textPrimary text-xs opacity-70 tracking-[0.2em] pb-8">
+              MENU
             </p>
-            <SocialLinks />
-          </div>
-        </Menu.Items>
+            <div className="flex flex-col gap-y-8 pb-14">
+              {menuItems.map(({ label, href }) => (
+                <Menu.Item key={label}>
+                  <Link href={href} passHref>
+                    <span className="font-bold text-textPrimary text-lg">
+                      {label}
+                    </span>
+                  </Link>
+                </Menu.Item>
+              ))}
+            </div>
+            <div>
+              <p className="font-bold text-textPrimary text-xs opacity-70 tracking-[0.2em] pb-10">
+                SOCIALS
+              </p>
+              <SocialLinks />
+            </div>
+          </Menu.Items>
+        )}
       </Transition>
     </Menu>
-  )
-}
+  );
+};
 
-export default DropdownMenu
+export default DropdownMenu;
