0508aad7378d7396c8f551136875d2d893a66a6e
Shamim Bin Nur <sbnsbn00@gmail.com>
2023-09-18 23:06:11 +0600

    Replaced previous hero image with an optimized version

--- components/sections/home-page/Hero.tsx
+++ components/sections/home-page/Hero.tsx
@@ -58,9 +58,9 @@ const Hero = ({ data }: HeroProps) => {
               </div>
             </div>
             
-            <div className="flex-1 ">
+            <div className="flex-1">
               <div>
-                <Image width={600} height={600} priority={true} loading="eager" className="relative scale-105 largeTablet:scale-110 -right-4" src="/hero.svg" alt="Insight Dashboard" />
+                <Image width={600} height={600} priority={true} loading="eager" className="relative scale-125 largeTablet:scale-125 scl -right-4" src="/hero.svg" alt="Insight Dashboard" />
               </div>
             </div>
           </div>
