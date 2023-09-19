633f40859b9978ae9b7298965de2eaa8ff0719ca
Shamim Bin Nur <48399373+shamimbinnur@users.noreply.github.com>
2023-09-20 04:17:50 +0600

    fix: Replaced hero image with an optimized version. (#165)

--- components/sections/home-page/Hero.tsx
+++ components/sections/home-page/Hero.tsx
@@ -8,6 +8,7 @@ import ContainerWithLine from '../../common/ContainerWithLine'
 import { Heading, Typography } from '../../common/text'
 import { Button } from '../../common'
 import DecoratedText from '../../common/text/utils/DecoratedText'
+import Image from 'next/image'
 interface HeroProps {
   data: SanityHomePage['hero']
 }
@@ -57,9 +58,9 @@ const Hero = ({ data }: HeroProps) => {
               </div>
             </div>
             
-            <div className="flex-1 ">
-              <div className="">
-                <img loading="eager" className="relative scale-105 largeTablet:scale-110 -right-4" src="/hero.svg" alt="" />
+            <div className="flex-1">
+              <div>
+                <Image width={600} height={600} priority={true} loading="eager" className="relative scale-125 largeTablet:scale-125 scl -right-4" src="/hero.svg" alt="Insight Dashboard" />
               </div>
             </div>
           </div>
