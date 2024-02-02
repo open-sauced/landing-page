90d19a88c27090c568fcc885632c0cf6580066f7
Zeu Capua <coding@zeu.dev>
2024-02-01 17:27:40 -0800

    change 404 copy

--- pages/404.tsx
+++ pages/404.tsx
@@ -28,10 +28,16 @@ export default function NotFound({ commonData } : {
       <SectionWrapper>
         <section className="items-center gap-y-12 z-50 py-24 tablet:py-56 border-slate-800 relative flex flex-col from-transparent via-red-800 to-transparent">
           <Heading component="h1" alignLarge="center">
-            $yellow-to-orange404$yellow-to-orange
+            $yellow-to-orange404: Page Not Found - But Opportunities are Everywhere$yellow-to-orange
           </Heading>
           <Typography alignSmall="center" variant="subheading">
-            Cannot find the page you're looking for!
+            It seems you've stumbled upon a path that doesn't exist... yet. If you believe this page should exist,  
+            <a 
+              className="text-brandYellow"
+              href="https://github.com/open-sauced/landing-page/issues/new?assignees=&labels=%F0%9F%91%80+needs+triage%2C%F0%9F%90%9B+bug&projects=&template=bug_report.yml&title=Bug%3A+">
+              {" let us know. "}
+            </a>
+            Your feedback is a valuable contribution to our community.
           </Typography>
           <Button href="/" fullWidth>
             Back to Homepage
