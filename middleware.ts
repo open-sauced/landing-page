4c695e6772b5d5024d127b4b2a63c62aeed182fc
Nick Taylor <nick@nickyt.co>
2024-03-12 16:47:21 -0400

    fix: now / rewrites, not redirects to /teams

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,9 @@ import { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   // TODO: see why this isn't working in async redirects in next.js config
   if (req.nextUrl.pathname === "/") {
-    return NextResponse.redirect(
-      new URL(
-        '/teams',
-        req.url
-      )
-    );
+    return NextResponse.rewrite(new URL(
+      '/teams',
+      req.url
+    ))
   }
 }
