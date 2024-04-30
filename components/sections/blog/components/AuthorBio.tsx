4aefe68192dda45449b30fb97288c9760406d952
Zeu Capua <coding@zeu.dev>
2024-04-30 04:35:12 -0700

    add top border

--- components/sections/blog/components/AuthorBio.tsx
+++ components/sections/blog/components/AuthorBio.tsx
@@ -3,14 +3,14 @@ import GradientBorderWrapper from "../../../common/GradientBorderWrapper";
 
 export default function AuthorBio({ author }: { author: SanityAuthor }) {
   return (
-    <section className="flex flex-col gap-4 mt-12 items-center pt-8">
-    <GradientBorderWrapper style={{ borderRadius: '9999px' }}>
-      <img 
-        src={author.portrait as unknown as string} 
-        alt={`${author.name} profile picture`} 
-        className="w-32 h-32 rounded-full"
-      />
-    </GradientBorderWrapper>
+    <section className="flex flex-col gap-4 mt-12 items-center pt-8 border-t-2 border-brandOrange">
+      <GradientBorderWrapper style={{ borderRadius: '9999px' }}>
+        <img 
+          src={author.portrait as unknown as string} 
+          alt={`${author.name} profile picture`} 
+          className="w-32 h-32 rounded-full"
+        />
+      </GradientBorderWrapper>
       <div className="flex flex-col items-center gap-2 w-full max-w-3xl px-4 lg:px-8">
         <h3 className="text-2xl font-bold">{author.name}</h3>
         <p className="text-gray50">{author.bio}</p>
