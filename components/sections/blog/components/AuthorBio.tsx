8c0a7102d87bc9a8161b9b19bc64a6c72ead143a
Zeu Capua <coding@zeu.dev>
2024-04-30 04:36:54 -0700

    use next <Image>

--- components/sections/blog/components/AuthorBio.tsx
+++ components/sections/blog/components/AuthorBio.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { SanityAuthor } from "../../../../types/schema";
 import GradientBorderWrapper from "../../../common/GradientBorderWrapper";
 
@@ -5,10 +6,12 @@ export default function AuthorBio({ author }: { author: SanityAuthor }) {
   return (
     <section className="flex flex-col gap-4 mt-12 items-center pt-8 border-t-2 border-brandOrange">
       <GradientBorderWrapper style={{ borderRadius: '9999px' }}>
-        <img 
+        <Image
           src={author.portrait as unknown as string} 
           alt={`${author.name} profile picture`} 
-          className="w-32 h-32 rounded-full"
+          width={128}
+          height={128}
+          className="rounded-full"
         />
       </GradientBorderWrapper>
       <div className="flex flex-col items-center gap-2 w-full max-w-3xl px-4 lg:px-8">
