a98321b22f607e6c052bb6ea30389360c44b52cd
Zeu Capua <coding@zeu.dev>
2024-04-29 19:28:20 -0700

    change to rerun deploy

--- components/sections/blog/components/AuthorBio.tsx
+++ components/sections/blog/components/AuthorBio.tsx
@@ -11,8 +11,8 @@ export default function AuthorBio({ author }: { author: SanityAuthor }) {
         className="w-32 h-32 rounded-full"
       />
     </GradientBorderWrapper>
-      <div className="flex flex-col gap-2 w-full max-w-3xl px-4 lg:px-8">
-        <h3 className="text-2xl text-center font-bold">{author.name}</h3>
+      <div className="flex flex-col items-center gap-2 w-full max-w-3xl px-4 lg:px-8">
+        <h3 className="text-2xl font-bold">{author.name}</h3>
         <p className="text-gray50">{author.bio}</p>
       </div>
     </section>
