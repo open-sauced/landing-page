ff5ceac720096cf1f1f12fdeb49925bc1b88dc96
isabensusan <isabensusan@gmail.com>
2024-05-20 01:37:39 -0300

    added images

--- components/sections/lunchweek/Launches.tsx
+++ components/sections/lunchweek/Launches.tsx
@@ -2,6 +2,7 @@ import React, { FC, useEffect, useState } from 'react'
 import SectionWrapper from '../../common/layout/SectionWrapper'
 import Launch from './Launch'
 
+
 interface LaunchesProps {
   totalLaunchesCount: number
   isLaunchesPage: boolean
@@ -22,7 +23,7 @@ const Launches: FC<LaunchesProps> = ({
         {
           title: 'Introductory Course for Maintainers',
           subtitle: 'Training Open Source Leaders and Maintainers.',
-          image: "https://cdn.sanity.io/images/r7m53vrk/production/7352f7c42cd131c3455710e6e24a643039367153-1687x838.png",
+          image: '/lunchweekImages/maintainer-course.png',
           launchDay: 'May 20th',
           docsLink: '',
           blogLink: '',
@@ -33,7 +34,7 @@ const Launches: FC<LaunchesProps> = ({
         {
           title: 'Workspaces 1.5',
           subtitle: 'Transform how you manage your open source projects.',
-          image: "https://cdn.sanity.io/images/r7m53vrk/production/7352f7c42cd131c3455710e6e24a643039367153-1687x838.png",
+          image: '/lunchweekImages/workspaces.png',
           launchDay: 'May 21st',
           docsLink: '',
           blogLink: '',
@@ -44,7 +45,7 @@ const Launches: FC<LaunchesProps> = ({
         {
           title: 'Repository Pages',
           subtitle: 'Explore the dynamics of any public GitHub repository—view up-to-date metrics, track popularity trends, and get a snapshot of the community’s health and activity.',
-          image: "https://cdn.sanity.io/images/r7m53vrk/production/7352f7c42cd131c3455710e6e24a643039367153-1687x838.png",
+          image: '/lunchweekImages/repository-pages.png',
           launchDay: 'May 22nd',
           docsLink: '',
           blogLink: '',
@@ -55,7 +56,7 @@ const Launches: FC<LaunchesProps> = ({
         {
           title: 'StarSearch',
           subtitle: 'Copilot for git history.',
-          image: "https://cdn.sanity.io/images/r7m53vrk/production/7352f7c42cd131c3455710e6e24a643039367153-1687x838.png",
+          image: '/lunchweekImages/star-search.png',
           launchDay: 'May 23rd',
           docsLink: '',
           blogLink: '',
