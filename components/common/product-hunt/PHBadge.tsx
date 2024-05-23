d8682d9184f9ef07c4da4b08646fc5a9084ac04b
Brian Douglas <bdougie@users.noreply.github.com>
2024-05-23 00:16:51 -0700

    Update PHBadge.tsx

--- components/common/product-hunt/PHBadge.tsx
+++ components/common/product-hunt/PHBadge.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PHBadge = () => {
   return (
     <div className="fixed z-50 bottom-6 right-6 tablet:bottom-8 tablet:right-9 ">
-      <a href="https://www.producthunt.com/posts/opensauced?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-opensauced"
+      <a href="https://www.producthunt.com/posts/starsearch?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-opensauced"
         target="_blank">
         <img
           className="w-36 tablet:w-60 tablet:h-12"
