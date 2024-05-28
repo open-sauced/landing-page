90dad192c8869502547c85d761e184e69a159bb8
Bekah Hawrot Weigel <rhawrot@gmail.com>
2024-05-28 15:13:49 -0400

    Update PH Badges

--- components/common/product-hunt/PHBadgeDark.tsx
+++ components/common/product-hunt/PHBadgeDark.tsx
@@ -3,16 +3,7 @@ import React from 'react'
 const PHBadgeDark = () => {
   return (
     <div className="w-fit -ml-2">
-      <a
-        href="https://www.producthunt.com/posts/starsearch?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-starsearch"
-        target="_blank"
-      >
-        <img
-          className="w-36 tablet:w-60 tablet:h-12"
-          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=455549&theme=dark"
-          alt="StarSearch - Copilot, but for git history | Product Hunt"
-        />
-      </a>
+     <a href="https://www.producthunt.com/posts/starsearch?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-starsearch" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=455549&theme=dark&period=daily" alt="StarSearch - AI Copilot but for git history | Product Hunt"  /></a>
     </div>
   )
 }
